a906d3d4f40df4e5a585f608c954062c0985927b
Nver Khachoyan <khachoyannver@gmail.com>
2023-04-15 22:30:17 -0700

    new thang

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>Nver Khachoyan's Blog</title>
         </Head>
         <Container>
           <Intro />
